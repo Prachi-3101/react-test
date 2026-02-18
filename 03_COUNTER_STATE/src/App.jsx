4e0c3fca41f6aeb219e387f0ce10d831aade1597
Prachi-3101 <prachistudies22@gmail.com>
2026-02-18 15:48:05 +0530

    New projects added

---
+++ 03_COUNTER_STATE/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function app(){
+const [count, setCount] = useState(0);
+const[countToSet,setCountToSet] = useState(0);
+
+  return(
+    <>
+    <h1>Counter is {count}</h1>
+   <div className="card">Count is {count}</div>
+   <div>
+    <button 
+    onClick={()=>{setCount(count+1)}}
+    style={{margin: "0 5px"}}>
+      Increase
+      </button>
+    <button 
+    onClick={()=>setCount((count)=>Math.max(count-1,0))}
+    style={{margin: "0 5px"}}>
+      Decrease 
+      </button>
+    <button 
+    onClick={()=>{setCount((count)=>0)}}
+    style={{margin: "0 5px"}}>
+      Reset
+      </button>
+   </div>
+   <div style={{margin:"20px 0" }}>
+    <input
+    style={{
+      width: "100px",
+      border: "1px solid white",
+      margin: "0 5px",
+      padding: "0.6em 1.2em",
+      borderRadius: "10px"
+    }}
+    value={countToSet}
+    onChange={(e)=>setCountToSet(Number(e.target.value))}
+    type="text" />
+    <button
+    style={{
+      margin: "0 5px",
+    }}
+    onClick={()=>{
+      setCount(Number(countToSet))
+      setCountToSet(0)
+    }}
+    >Set to {countToSet}</button>
+   </div>
+    </>
+    
+  )
+}
