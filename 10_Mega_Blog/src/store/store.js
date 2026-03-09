eb271531a80909dd17323191dc1d7637044c771e
Prachi-3101 <prachistudies22@gmail.com>
2026-03-09 17:09:18 +0530

    configured redux toolkit

---
+++ 10_Mega_Blog/src/store/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {
+        tasks: taskReducer
+    },
+    devTools: true
+});
+
+export default store;
